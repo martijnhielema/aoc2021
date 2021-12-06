0cae5bdaa62e88aa35dbe103e52405c99fb1aa8f
Martijn Hielema <martijn.hielema@sogeti.com>
2021-12-06 08:57:08 +0100

    day 5

--- day5/day5.py
+++ day5/day5.py
@@ -23,20 +23,39 @@ for i in coordinates:
     if i[1][1] > max_x:
         max_y = i[1][1]
 
-print(max_x, max_y)
-thermals = np.zeros((max_x + 2, max_y + 2))
-print(np.shape(thermals))
-for i in coordinates:
-    x1 = i[0][0]
-    y1 = i[0][1]
-    x2 = i[1][0]
-    y2 = i[1][1]
 
-    if y1 == y2:
-        for j in range(min(x1, x2), max(x1, x2) + 1):
-            thermals[j][y1] += 1
-    elif x1 == x2:
-        for j in range(min(y1, y2), max(y1, y2) + 1):
-            thermals[x1][j] += 1
+def horizontal_line(from_coordinate, to_coordinate):
+    line = []
+    for i in range(from_coordinate[0], to_coordinate[0] + 1):
+        line.append((i, from_coordinate[1]))
+
+    return line
+
+
+def vertical_line(from_coordinate, to_coordinate):
+    line = []
+    for i in range(from_coordinate[1], to_coordinate[1] + 1):
+        line.append((from_coordinate[0], i))
+
+    return line
+
+
+thermals = np.zeros((max_x + 2, max_y + 2))
+for i in coordinates:
+    from_coordinate = i[0]
+    to_coordinate = i[1]
+    from_x = from_coordinate[0]
+    from_y = from_coordinate[1]
+    to_x = to_coordinate[0]
+    to_y = to_coordinate[1]
+
+    if from_y == to_y:
+        line_coordinates = horizontal_line(from_coordinate, to_coordinate)
+    elif from_x == to_x:
+        line_coordinates = vertical_line(from_coordinate, to_coordinate)
+
+    for coordinate in line_coordinates:
+        thermals[coordinate[0]][coordinate[1]] += 1
+
 
 print(np.count_nonzero(thermals >= 2))
